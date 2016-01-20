d52ef36be2faec8be3a414a1ad74552a95f7c3c6
jame5380 <jame5380@ISS147D8-24.scdsb.pri>
2016-01-20 10:30:52 -0500

    ISU

--- src/Demon.java
+++ src/Demon.java
@@ -1,13 +1,20 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author jame5380
- */
-public class Demon {
+import TurtleGraphics.Pen;
+
+
+public class Demon extends AbstractDemon{
+
+    public Demon(){
+        super(0,0);
+    }
+    
+    public Demon(int x, int y){
+        super(x,y);
+    }
+    
+    @Override
+    public void draw(Pen p) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
